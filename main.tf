793c5fe7b242df3a1db27dd9b0d63e33eb498f9b
k-seta <swordh16@gmail.com>
2021-10-02 13:14:09 +0900

    main.tfを作成

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = " fredboat-selfhost-327804"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-backend-809237"
+  }
+}
+
+resource "google_compute_instance" "default" {
+  name         = "fredboat"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/cos-cloud/global/images/family/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
