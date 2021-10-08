3e135ed7920fe4fc3e33ea5fac0a171f3728ed19
k-seta <swordh16@gmail.com>
2021-10-09 01:54:24 +0900

    twinproduction/discord-music-botを導入

--- startup.sh
+++ startup.sh
@@ -3,17 +3,6 @@
 mkdir -p /home
 cd /home
 
-git clone https://github.com/freyacodes/archived-bot.git
-cd archived-bot/config/templates
-
 export DISCORD_BOT_TOKEN=$(curl -H "Metadata-Flavor: Google" 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/discord-bot-token')
 
-mv selfhosting.yml docker-compose.yml
-
-sed -i -e "s/discordToken: \"\"/discordToken: ${DISCORD_BOT_TOKEN}/g" common.yml
-
-docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:$PWD" \
-    -w="$PWD" \
-    docker/compose:1.24.0 up
+docker run -e DISCORD_BOT_TOKEN=${DISCORD_BOT_TOKEN} -e MAXIMUM_AUDIO_DURATION_IN_SECONDS=3600 --name discord-music-bot --rm twinproduction/discord-music-bot
