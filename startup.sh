1127812938692895bd1198146e2fb4147c607040
k-seta <swordh16@gmail.com>
2022-11-03 12:45:35 +0900

    dockerイメージをlatestに変更

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ cd /home
 export DISCORD_BOT_TOKEN=$(curl -H "Metadata-Flavor: Google" 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/discord-bot-token')
 export YOUTUBE_API_KEY=$(curl -H "Metadata-Flavor: Google" 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/youtube-api-key')
 
-docker run -e TOKEN=${DISCORD_BOT_TOKEN} -e YOUTUBE_API_KEY=${YOUTUBE_API_KEY} -e PREFIX='!' eritislami/evobot:master
+docker run -e TOKEN=${DISCORD_BOT_TOKEN} -e YOUTUBE_API_KEY=${YOUTUBE_API_KEY} -e PREFIX='!' eritislami/evobot:latest
